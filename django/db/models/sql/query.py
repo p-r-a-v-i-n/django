a1eff295df7579f178fd068c60e78d8fbd5ca9f7
Pravin Kamble <iampbkamble@gmail.com>
2026-07-30 17:11:12 +0530

    Fixed reassignment of materialized table source aliases.

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -1380,6 +1380,7 @@ class Query(BaseExpression):
                 for join in self.alias_map.values()
                 if isinstance(join, SetReturningFunctionJoin)
                 and join.table_name == alias
+                and join.srf_func == annotation
             ),
             None,
         )
